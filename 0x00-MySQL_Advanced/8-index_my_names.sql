cd61c57f63f325a8244facf3f52674b85f1bd363
LoayAbassi <abassi.loay@gmail.com>
2024-10-21 17:25:12 +0100

    left no left

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,4 @@
 -- creates index on the 1st letter of names(name)
+-- create index idx_name_first on names((left(name,1)));
+-- works but slower cz it'll have to call left() function
 create index idx_name_first on names(name(1));

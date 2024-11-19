0bfdc615098b29358a92539199f633c40e1b0dd1
LoayAbassi <abassi.loay@gmail.com>
2024-11-19 11:21:12 +0100

    bellehy emchi

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""retreiving the collection and returning documents"""
+
+
 def list_all(mongo_collection):
     """returns documents of collection"""
     if mongo_collection.find().count() == 0:

d072d909addee9b412fc243ae75b93c5615db2ad
LoayAbassi <abassi.loay@gmail.com>
2024-11-19 11:12:41 +0100

    documentation, logic, pycodestyle

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 def list_all(mongo_collection):
+    """returns documents of collection"""
+    if mongo_collection.find().count() == 0:
+        return []
     return mongo_collection.find()
